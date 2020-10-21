30c027abd60b4f90760133b403728d6979e224b8
Tekhamian <tekhamian@gmail.com>
2020-10-21 15:48:26 -0400

    Innitial React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    // empty element (<> = fragment) is all we need for this section right now.
+    <>
+      <h1>Welcome to the Store Front</h1>
+    </>
+  );
+}
+
+export default App;
